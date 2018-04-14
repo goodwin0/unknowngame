3307415967b05589cac834905b2ab15b9f41e3fe
Mubarakshin <sabir.mubarakshin@gmail.com>
2018-04-14 15:52:07 +0400

    first commit

---
+++ android/src/com/unknowngame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.unknowngame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unknowngame.UnknownGameRunner;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UnknownGameRunner(), config);
+	}
+}
---
+++ desktop/src/com/unknowngame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unknowngame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unknowngame.UnknownGameRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UnknownGameRunner(), config);
+	}
+}
